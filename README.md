9315eea858873b21e139ab30e0d57395f68a5ac7
tcbegley <tomcbegley@gmail.com>
2020-10-27 18:06:11 +0000

    First commit - setup project structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hexplode
+
+A pluggable Python implementation of a BBC Micro classic. I made this to
+experiment with and learn about game playing AIs.
---
+++ noxfile.py
@@ -0,0 +1,23 @@
+import nox
+
+SOURCES = ["src", "tests", "noxfile.py"]
+
+
+@nox.session()
+def lint(session):
+    """Lint Python source"""
+    session.install("black", "flake8", "isort", "mypy")
+    session.run("black", "--check", *SOURCES)
+    session.run("flake8", *SOURCES)
+    session.run("isort", "--check", *SOURCES)
+    session.run("mypy", *SOURCES)
+
+
+@nox.session(python=["3.7", "3.8", "3.9"])
+def test(session):
+    """Run tests"""
+    session.install("poetry")
+    env = {"VIRTUAL_ENV": session.virtualenv.location}
+    session.run("poetry", "install", "--no-dev", env=env, external=True)
+    session.install("pytest")
+    session.run("pytest")
